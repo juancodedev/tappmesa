5dbf16548050437a2fa4a050742f71ec5a3c29d7
Juan Muñoz <cl.jmunoz@gmail.com>
2025-10-09 00:36:08 -0300

    tailwind app integrate

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
   define: {
     'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
     'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
